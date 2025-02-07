99139e261e0ba4a0850eeaca1457212886431a51
DESKTOP-25SH4IH\전산1 <tkwkdlq@gmail.com>
2025-02-07 09:14:04 +0900

    소스트리 커밋 테스트

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, send_from_directory
+import os
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/consent')
+def consent():
+    selected = request.args.get("selected", "").split(",")
+    return render_template("consent.html", selected=selected)
+
+# 정적 파일 (PDF) 제공 라우트
+@app.route('/static/pdfs/<path:filename>')
+def serve_pdf(filename):
+    return send_from_directory(os.path.join(app.static_folder, "pdfs"), filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
